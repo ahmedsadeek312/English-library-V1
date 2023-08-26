6879fac75112d225f136ffa6443544eb320ecb0f
ahmed <ahmedsadeek312@gmail.com>
2023-08-27 01:44:33 +0300

    deploy 6

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
 import BookList from "./components/BookList";
+import "bootstrap/dist/css/bootstrap.min.css";
 import BookDetails from "./components/BookDetails";
 import { AppProvider } from "./APIFetch";
+import NotFound from "./components/NotFound";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="about" element={<About />} />
           <Route path="book" element={<BookList />} />
           <Route path="/book/:id" element={<BookDetails />} />
         </Route>
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </AppProvider>
